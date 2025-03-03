17dfb7dc2bec2bcfbf15a2f41e461192cf2647c5
ita Dev <itajacy@gmail.com>
2025-03-03 07:50:39 -0300

    implementacao das animacoes de opacidade e exeibicao do Container()

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,31 +29,67 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _scrollController = ScrollController();
-  
+
+  var scrollFactor = 0.0;
+
   @override
   void initState() {
     super.initState();
 
     _scrollController.addListener(() {
-
       final percent =
           _scrollController.position.pixels /
           _scrollController.position.maxScrollExtent;
       // print(_scrollController.position.maxScrollExtent);
       // print(_scrollController.position.pixels);
-      print(percent);
+      // print(percent);
+
+      setState(() {
+        scrollFactor = percent;
+      });
     });
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+
+    double opacityAnimation = Interval(0.2, 0.4).transform(scrollFactor);
+    double translateYFactor = Interval(0.2, 0.4).transform(scrollFactor);
+    double? translateYAnimation = lerpDouble(-50, 30, translateYFactor);
+
     return Material(
-      child: ListView.builder(
-        controller: _scrollController,
-        itemCount: 100,
-        itemBuilder: (context, index) {
-          return ListTile(title: Text('Item $index'));
-        },
+      child: Stack(
+        children: [
+          ListView.builder(
+            controller: _scrollController,
+            itemCount: 100,
+            itemBuilder: (context, index) {
+              return ListTile(title: Text('Item $index'));
+            },
+          ),
+          Positioned(
+            bottom: translateYAnimation,
+            right: 30,
+            child: Opacity(
+              opacity: opacityAnimation,
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(Icons.arrow_upward_outlined, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
